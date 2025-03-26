be00fa68846eb4e4a2cefd5af38bc0a05678eeb6
Brandon Williams <b_williams20@mail.fhsu.edu>
2025-03-26 19:59:16 -0400

    Fixed index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo '<body style="background-color:#f9f9f9;">';
 echo "<h2>INF653 Midterm Project API</h2>";
-echo "<h4>By: Brandon Williams\n</h4>"
+echo "<h4>By: Brandon Williams\n</h4>";
 echo "<p>Welcome! This REST API serves quotes from Christian scholars.\n</p>";
 echo "<p>Make sure to check them out here:</p>";
 echo "<ul>";
