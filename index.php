a1970c8ec06cfe3f0d045ee9ee510f728507f941
Brandon Williams <b_williams20@mail.fhsu.edu>
2025-03-26 19:56:13 -0400

    Added main index.php landing page

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo '<body style="background-color:#f9f9f9;">';
+echo "<h2>INF653 Midterm Project API</h2>";
+echo "<h4>By: Brandon Williams\n</h4>"
+echo "<p>Welcome! This REST API serves quotes from Christian scholars.\n</p>";
+echo "<p>Make sure to check them out here:</p>";
+echo "<ul>";
+echo "<li><a href='/api/quotes/'>/api/quotes/</a></li>";
+echo "<li><a href='/api/authors/'>/api/authors/</a></li>";
+echo "<li><a href='/api/categories/'>/api/categories/</a></li>";
+echo "</ul>";
+?>
